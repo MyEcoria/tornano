9b08600a31eca875638518dc011f13f8e94f10cb
MyEcoria <github@myecoria.com>
2023-06-26 12:58:27 +0200

    V1.2

--- start/index.js
+++ start/index.js
@@ -6,6 +6,7 @@ const { wallet: walletLib } = require('multi-nano-web');
 const { saveTransaction, getCurrentCycle, createAccount, changeAccountStatus, getAccountStatus, saveSeed } = require('../modules/db');
 const cors = require('cors');
 const cliProgress = require('cli-progress');
+const { send } = require('../modules/send');
 
 // Configs
 const serverConf = require('../config/server.json');
@@ -22,55 +23,84 @@ function isNanoAddress(address) {
   return regex.test(address);
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 app.post('/create', async (req, res) => {
   const data = req.body;
 
-  if (data.to && data.cycles && data.decalage >= 0) {
-    // Vérifier si data.to est une adresse Nano valide
-    const nanoAddressRegex = /^(nano|xrb)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$/i;
-    if (!nanoAddressRegex.test(data.to)) {
-      return res.status(400).json({ error: 'Adresse Nano invalide' });
-    }
-
-    let seed = walletLib.generateLegacy().seed;
-    saveSeed(seed);
-    const wallet = walletLib.fromLegacySeed(seed);
-    // Traitez les données comme vous le souhaitez
-    res.status(200).json({ deposit: wallet.accounts[0].address });
-    createAccount(wallet.accounts[0].address);
-
-    try {
-      const result = await analyse(seed, 7200000);
-      if (result.status == 'ok') {
-        changeAccountStatus(wallet.accounts[0].address);
-        const currentCycle = await getCurrentCycle();
-
-        const progressBar = new cliProgress.SingleBar({
-          format: 'Progress |{bar}| {percentage}% | ETA: {eta_formatted} | {value}/{total}',
-          barCompleteChar: '\u2588',
-          barIncompleteChar: '\u2591',
-          hideCursor: true
-        });
-
-        progressBar.start(data.cycles, 0);
-
-        for (let i = 0; i < data.cycles; i++) {
-          const dividedNumber = result.data / data.cycles;
-          const randomPercentage = Math.random() * (0.05 - 0.001) + 0.001; // Génère un pourcentage aléatoire entre 0.1% et 5%
-          const deductedNumber = Math.floor(dividedNumber * (1 - randomPercentage))
-            .toLocaleString(undefined, { maximumFractionDigits: 0 })
-            .replace(/,/g, ''); // Effectue la déduction
-
-          const cycle = currentCycle + data.decalage + i;
-          await saveTransaction(data.to, deductedNumber, cycle);
-
-          progressBar.increment();
-        }
-
-        progressBar.stop();
+  if (data.type == 1){
+    if (data.to && data.cycles && data.decalage >= 0) {
+      // Vérifier si data.to est une adresse Nano valide
+      const nanoAddressRegex = /^(nano|xrb)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$/i;
+      if (!nanoAddressRegex.test(data.to)) {
+        return res.status(400).json({ error: 'Adresse Nano invalide' });
       }
-    } catch (error) {
-      console.log('{ "status": "error" }');
+  
+      let seed = walletLib.generateLegacy().seed;
+      saveSeed(seed);
+      const wallet = walletLib.fromLegacySeed(seed);
+      // Traitez les données comme vous le souhaitez
+      res.status(200).json({ deposit: wallet.accounts[0].address });
+      createAccount(wallet.accounts[0].address);
+  
+      try {
+        const result = await analyse(seed, 7200000);
+        if (result.status == 'ok') {
+          changeAccountStatus(wallet.accounts[0].address);
+          const currentCycle = await getCurrentCycle();
+  
+          const progressBar = new cliProgress.SingleBar({
+            format: 'Progress |{bar}| {percentage}% | ETA: {eta_formatted} | {value}/{total}',
+            barCompleteChar: '\u2588',
+            barIncompleteChar: '\u2591',
+            hideCursor: true
+          });
+  
+          progressBar.start(data.cycles, 0);
+  
+          for (let i = 0; i < data.cycles; i++) {
+            const dividedNumber = result.data / data.cycles;
+            const randomPercentage = Math.random() * (0.05 - 0.001) + 0.001; // Génère un pourcentage aléatoire entre 0.1% et 5%
+            const deductedNumber = Math.floor(dividedNumber * (1 - randomPercentage))
+              .toLocaleString(undefined, { maximumFractionDigits: 0 })
+              .replace(/,/g, ''); // Effectue la déduction
+  
+            const cycle = currentCycle + data.decalage + i;
+            await saveTransaction(data.to, deductedNumber, cycle);
+  
+            progressBar.increment();
+          }
+  
+          progressBar.stop();
+        }
+      } catch (error) {
+        console.log('{ "status": "error" }');
+      }
+    }
+  } else if (data.type == 2) {
+    if (data.to) {
+      const nanoAddressRegex = /^(nano|xrb)_[13]{1}[13456789abcdefghijkmnopqrstuwxyz]{59}$/i;
+      if (!nanoAddressRegex.test(data.to)) {
+        return res.status(400).json({ error: 'Adresse Nano invalide' });
+      }
+      let seed = walletLib.generateLegacy().seed;
+      saveSeed(seed);
+      const wallet = walletLib.fromLegacySeed(seed);
+      // Traitez les données comme vous le souhaitez
+      res.status(200).json({ deposit: wallet.accounts[0].address });
+      createAccount(wallet.accounts[0].address);
+
+      try {
+        const result = await analyse(seed, 7200000);
+        if (result.status == 'ok') {
+          changeAccountStatus(wallet.accounts[0].address);
+          await send(data.to, result.data);
+        }
+      } catch (error) {
+        console.log('{ "status": "error" }');
+      }
     }
   }
 });
--- start/receive.js
+++ start/receive.js
@@ -44,4 +44,4 @@ async function receive() {
 receive();
 
 // Exécuter la fonction receive() toutes les 5 minutes
-setInterval(receive, 5 * 60 * 1000);
+setInterval(receive, 1 * 60 * 1000);
